e794455111661c4021bd8cbf0aba856a490d8c5b
Kyle Henry <krhenry433@gmail.com>
2018-12-14 16:02:47 -0500

    Initial

---
+++ fire-base-auth-start/src/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAIR5N9cJHtM9oEVntgBxGVhWjZq-pyxHU',
+    authDomain: 'ng-firebase-intro.firebaseapp.com',
+    databaseURL: 'https://ng-firebase-intro.firebaseio.com',
+    projectId: 'ng-firebase-intro',
+    storageBucket: 'ng-firebase-intro.appspot.com',
+    messagingSenderId: '147570906461'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
